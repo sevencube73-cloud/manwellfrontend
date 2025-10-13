0ef5f76f84c9a55d3d63e0b3c704d211e77ec81b
Muzamilafey <muzamilinc@gmail.com>
2025-10-13 03:52:13 +0300

    gfhjhgb

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../../context/CartContext';
-import './ShopItemCard.css';
+import './ProductCard.css';
 
 const ProductCard = ({ product }) => {
   const [qty, setQty] = useState(1);
