f640d8a55f03fbd0191c58aa3c256180c83ff0df
Muzamilafey <muzamilinc@gmail.com>
2025-10-16 00:01:29 +0300

    gdf

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import ProductList from '../components/product/ProductList';
-import './HomePage.css';
-import api from '../utils/api'; // axios instance
+import React from "react";
+import ProductList from "../components/product/ProductList";
+import "./HomePage.css";
 
 const HomePage = () => {
   return (
     <div className="products-section">
       {/* Hero Section */}
-      <section className="">
+      <section>
         <h2 className="section-title">MANWELL STORE</h2>
         <ProductList />
       </section>
