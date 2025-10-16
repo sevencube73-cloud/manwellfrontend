25b26cfd6abb6afc3d6ca3110a7d5d4fec72699f
Muzamilafey <muzamilinc@gmail.com>
2025-10-17 00:48:35 +0300

    sffs

--- src/pages/resetPassword.js
+++ src/pages/resetPassword.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import "./ResetPassword.css";
+import "./pages.css";
 
 const ResetPassword = () => {
   const location = useLocation();
